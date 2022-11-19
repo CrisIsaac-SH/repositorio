3934f45d9f0d5c65fe42510323d561443ca5157f
CrisIsaac-SH <98175694+CrisIsaac-SH@users.noreply.github.com>
2022-11-19 01:11:00 +0000

    mejorado ahorita

--- ProcessScheduler.java
+++ ProcessScheduler.java
@@ -13,27 +13,45 @@ public class ProcessScheduler{
         SimpleProcess.ConditionalTime = 3;
         SimpleProcess.IterativoTime = 4;
 
-
-        //Policy politica1= new FCFS();
-        Policy politica2 = new LCFS();
-//
         SimpleProcess aritmetico = new Aritmetico(1);
         SimpleProcess inputOutput = new InputOutput(2);
         SimpleProcess condiciones = new Condicionales(3);
         SimpleProcess iterativos = new Iterativos(4);
-//
+        ///////////////
+        SimpleProcess aritmetico1 = new Aritmetico(5);
+        SimpleProcess inputOutput2 = new InputOutput(6);
+        SimpleProcess condiciones3 = new Condicionales(7);
+        SimpleProcess iterativos4 = new Iterativos(8);
+
+        ///////////////
+
+
+        Policy politica1= new FCFS();
         //Server servidor = new Server(politica1);
-        Server servidorr = new Server(politica2);
+        Server servidor1 = new Server(politica1);
+        politica1.add(aritmetico);
+        politica1.add(inputOutput);
+        politica1.add(condiciones);
+        politica1.add(iterativos);
 
-        politica2.add(aritmetico);
-        politica2.add(inputOutput);
-        politica2.add(condiciones);
-        politica2.add(iterativos);
+        
 
-        servidorr.serve();
+        Server servidor2 = new Server(politica1);
+        politica1.add(aritmetico1);
+        politica1.add(inputOutput2);
+        politica1.add(condiciones3);
+        politica1.add(iterativos4);
+        
+        Thread t = new Thread(servidor1);
+        t.start();
         
         
 
+        Thread t2 = new Thread(servidor2);
+        t2.start();
+
+        //servidor1.run();
+        //servidor2.run();
 
         System.out.println("tenemos que ejecutar\n");
         
--- Server.java
+++ Server.java
@@ -1,21 +1,27 @@
-public class Server {
+public class Server implements Runnable {
 
-
+    int cont = 1;
     private Policy policy;
     private boolean active; // Esto se va a desactivar al presionar  Q o enter
+    private int id;
+    public static int numeroServer;
     public Server(Policy policy){
         this.policy= policy;
         this.active = true; 
+        this.id = Server.numeroServer;
+        this.id++;
     }
 
-    public void serve(){
+    public void run(){
         SimpleProcess actualProcess;
         while (policy.isEmpty()==false){
             actualProcess=  policy.next();
             policy.serveNext();
+            int total = cont++;
             if(actualProcess.isFinished()){
                 policy.remove(actualProcess);    
-                System.out.println(" finalizo el proceso");
+                System.out.println(" finalizo el proceso de servidor "+ this.id);
+                System.out.println("\nTotal procesos: " + total + "\n"); 
             }else{
                 System.out.println("Aun no se finalizo");
             }
